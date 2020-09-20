f58365747ba23c48fbbac55378948d765268bd9c
Grigor <gmmarmenia@gmail.com>
2020-09-20 16:27:03 +0400

    Add files via upload

---
+++ insertion sort.cpp
@@ -0,0 +1,25 @@
+
+48/4000 = 0.012
+
+0.012 0.048  0.012 0.048
+
+48 192  48 192
+
+i =192
+j =192/4000 =0.048
+
+replace(192, j) -> 48 0.048 48 0.048
+
+i = 48
+j = 48/4000 =
+
+
+
+
+
+rm -rf scratch/ ; build_cells -config config_ts16ncfslogl16hdp090f_gds > bcc_only_gds;
+
+
+
+1.  \bHDB.+ECO.+ 
+2. HDB.+(?<!FILL1)$
