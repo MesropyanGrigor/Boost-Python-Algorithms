eb6a7ee617c1ffd66ae42a64c59d1b979315d42b
Grigor <gmmarmenia@gmail.com>
2020-09-20 19:08:41 +0400

    Add files via upload

--- insertion sort.cpp
+++ insertion sort.cpp
@@ -1,25 +1,22 @@
-// C++ program for insertion sort  
-//#include <bits/stdc++.h> 
+// BOOST::PYTHON C++ program for insertion sort  
 #include <boost/python.hpp>
 #include <iostream>
 
 using namespace std; 
-//using namespace py = boost::python;
-namespace py = boost::python;
+namespace BP = boost::python;
   
 /* Function to sort an array using insertion sort*/
 
 
 template<typename T>
-void insertionSort(boost::python::list arr)  
+void insertionsort(BP::list arr) 
 {  
-    int n = boost::python::len(arr);
-    //boost::python::list s_list;
+    int n = BP::len(arr);
     int j;  
     T key;
     for (int i = 1; i < n; i++) 
     {  
-        key = boost::python::extract<T>(arr[i]);  
+        key = BP::extract<T>(arr[i]);  
         j = i - 1;  
         while (j >= 0 && arr[j] > key) 
         {  
@@ -31,32 +28,18 @@ void insertionSort(boost::python::list arr)
 }  
   
 // A utility function to print an array of size n  
-void printarray(boost::python::list arr)  
+void printarray(BP::list arr)  
 {  
-    for (int i = 0; i < boost::python::len(arr); i++)  
-        cout << py::extract<int>(arr[i])<< " ";  
+    for (int i = 0; i < BP::len(arr); i++)  
+        cout << BP::extract<int>(arr[i])<< " ";  
     cout << endl; 
 }  
- /*** 
-// Driver code 
-int run()  
-{  
-    int arr[] = { 12, 11, 13, 5, 6 };  
-    int n = sizeof(arr) / sizeof(arr[0]);  
-  
-    insertionSort(arr, n);  
-    printArray(arr, n);  
-  
-    return 0;  
-}  
-  
-*/
 
 BOOST_PYTHON_MODULE(iso)
 {
-	boost::python::def("insertionsort", &insertionSort<int>,
-                           py::args("arr"), 
-                           "Insertion sort algorithm");
-        boost::python::def("printarray", &printarray);
-	//boost::python::def("run", &run);
+	BP::def("insertionsort",
+                &insertionsort<int>,
+                BP::args("arr"), 
+                "Insertion sort algorithm");
+        BP::def("printarray", &printarray);
 }
