963893d3dc3073ff40d850f4fc46874422925d69
Grigor <gmmarmenia@gmail.com>
2020-09-22 20:30:11 +0400

    Add files via upload

--- insertion sort.cpp
+++ insertion sort.cpp
@@ -35,11 +35,74 @@ void printarray(BP::list arr)
     cout << endl; 
 }  
 
+
+// A utility function to swap two elements  
+//void swap(int* elem_1, int* elem_2)  
+
+void swap1(auto elem_1, auto elem_2)  
+{  
+    cout<<BP::extract<int>(elem_1)<< "---"<< BP::extract<int>(elem_2)<<endl;
+    auto tmp = elem_1;  
+    elem_1 = elem_2;  
+    elem_2 = tmp;  
+    cout<<BP::extract<int>(elem_1)<< "---"<< BP::extract<int>(elem_2)<<endl;
+}  
+  
+void swap(auto elem_1, auto elem_2)  
+{  
+    cout<<BP::extract<int>(elem_1)<< "---"<< BP::extract<int>(elem_2)<<endl;
+    BP::object tmp = elem_1;  //BP::extract<int>(elem_1);  
+    elem_1 = elem_2;  
+    elem_2 = tmp; //BP::object(tmp);  
+    cout<<BP::extract<int>(elem_1)<< "---"<< BP::extract<int>(elem_2)<<endl;
+}  
+/* This function takes last element as pivot, places  
+the pivot element at its correct position in sorted  
+array, and places all smaller (smaller than pivot)  
+to left of pivot and all greater elements to right  
+of pivot */
+int partition(BP::list arr, int low, int high)  
+{  
+    int pivot = BP::extract<int>(arr[high]); // pivot element
+    int i = (low - 1); // Index of smaller element  
+    for (int j = low; j <= high - 1; j++)  
+    {  
+        // If current element is smaller than the pivot  
+        if (arr[j] < pivot)  
+        {  
+            i++; // increment index of smaller element  
+	    cout<<typeid(arr[i]).name()<<endl;
+            swap(arr[i], arr[j]);  
+        }  
+    }  
+    swap(arr[i + 1], arr[high]);  
+    return (i + 1);  
+}  
+  
+/* The main function that implements QuickSort  
+arr[] --> Array to be sorted,  
+low --> Starting index,  
+high --> Ending index */
+void quickSort(BP::list arr, int low, int high)  
+{  
+    if (low < high)  
+    {  
+        int mid_i = partition(arr, low, high);  
+        quickSort(arr, low, mid_i - 1);  
+        quickSort(arr, mid_i + 1, high);  
+    }  
+}  
+
+void quicksort(BP::list arr)
+{
+	quickSort(arr, 0, BP::len(arr) - 1);
+}
+  
+// This code is contributed by rathbhupendra 
+
 BOOST_PYTHON_MODULE(iso)
 {
-	BP::def("insertionsort",
-                &insertionsort<int>,
-                BP::args("arr"), 
-                "Insertion sort algorithm");
+	BP::def("insertionsort", &insertionsort<int>, BP::args("arr"), "Insertion sort algorithm");
+	BP::def("qsort", &quicksort);
         BP::def("printarray", &printarray);
 }
